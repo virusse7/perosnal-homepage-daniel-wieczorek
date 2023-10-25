ce864e97b19d5c45e0f09fb23f97bcab997f2d2a
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-25 20:13:28 +0200

    added globalstyles to project

--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -1,5 +1,24 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
 
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        font-family: 'Inter', sans-serif;
+        background: ${({ theme }) => theme.colors.site.background};
+        color: ${({ theme }) => theme.colors.site.text};
+        padding-bottom: 108px;
+        word-break: break-word;
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+            padding-bottom: 32px;
+        }
+
+    }
 `;
