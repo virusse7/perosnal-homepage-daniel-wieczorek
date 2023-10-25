2fddfe90301c3e7f0d664c48a3146461bf73cd7d
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-25 21:26:54 +0200

    added skills to table

--- src/features/PersonalHomepage/skillsData.js
+++ src/features/PersonalHomepage/skillsData.js
@@ -1,7 +1,23 @@
 export const skills = [
-
+    "Responsive Web Design",
+    "Teamwork",
+    "Markdown",
+    "CSS BEM convention",
+    "CSS Grid",
+    "CSS Flexbox",
+    "React Router",
+    "JavaScript: ES6+",
+    "NPM",
+    "React",
+    "Git",
+    "Immutability",
+    "Semantic & accessible HTML",
 ];
 
 export const nextSkills = [
-
+    "Node.js",
+    "TypeScript",
+    "Redux-Saga (improve skills in this section)",
+    "Working with API (improve skills in this section)",
+    "React Context",
 ];
