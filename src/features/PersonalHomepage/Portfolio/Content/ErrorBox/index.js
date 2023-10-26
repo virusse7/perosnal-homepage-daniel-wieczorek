b1c9d4bf1020542e0c0aa8f187bb662817c69815
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-26 17:39:46 +0200

    updated error box

--- src/features/PersonalHomepage/Portfolio/Content/ErrorBox/index.js
+++ src/features/PersonalHomepage/Portfolio/Content/ErrorBox/index.js
@@ -1,12 +1,18 @@
 import { ReactComponent as DangerIcon } from "./Danger.svg";
 import { ButtonLink } from "../../../ButtonLink";
 import { Wrapper, Header, Paragraph } from "./styled";
+import { githubUsername } from "../../githubUsername";
 
 export const ErrorBox = () => {
-    <Wrapper>
-        <DangerIcon />
-        <Header></Header>
-        <Paragraph></Paragraph>
-        <ButtonLink></ButtonLink>
-    </Wrapper>
-}
+    return (
+        <Wrapper>
+            <DangerIcon />
+            <Header>Ooops! Something went wrong...</Header>
+            <Paragraph>
+                Sorry, failed to load Github projects.
+                You can check them directly on Github.
+            </Paragraph>
+            <ButtonLink href={`https://github.com/${githubUsername}`} target="_blank" rel="noreferrer">Go to Github</ButtonLink>
+        </Wrapper>
+    );
+};
