8e0bc3e162542b7f92ab0d3309b3d1f7ebe07170
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-26 18:01:18 +0200

    updated button with dispatch function

--- src/common/ThemeSwitch/index.js
+++ src/common/ThemeSwitch/index.js
@@ -1,18 +1,18 @@
-// import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Wrapper, Button, Text, Box, IconWrapper, Icon } from "./styled";
+import { selectIsDarkTheme, toggleTheme } from "../themeSlice";
 
 export const ThemeSwitch = () => {
-    // const dispatch = useDispatch();
-    // const isDarkTheme = useSelector();
+    const isDarkTheme = useSelector(selectIsDarkTheme);
+    const dispatch = useDispatch();
     return (
         <Wrapper>
-            <Button>
+            <Button onClick={() => dispatch(toggleTheme())}>
                 <Text>
-                    Dark mode
-                    {/* {isDarkTheme ? "on" : "off"} */}
+                    Dark mode {isDarkTheme ? "on" : "off"}
                 </Text>
                 <Box>
-                    <IconWrapper>
+                    <IconWrapper moveToRight={isDarkTheme}>
                         <Icon />
                     </IconWrapper>
                 </Box>
